5de61f3a5c65f88ff821666ac51741369cb35098
llorracc <llorracc@users.noreply.github.com>
2019-03-26 12:46:03 -0400

    Add do_all.py

---
+++ do_all.py
@@ -0,0 +1,10 @@
+# do_all.py is required for a REMARK; in this case, it just runs TractableBufferStock.py
+
+import os
+os.chdir('./Code/HARK/Examples/ctDiscrete')
+
+import TractableBufferStockModel.py
+
+
+
+
